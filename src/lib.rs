c8bbb3aa449f4308b1a2570b3c6e316787943855
Lucianna from Elsewhere <something-from-elsewhere@proton.me>
2026-09-15 15:02:31 -0700

    Addition implemented, as well as conversion from wider/narrower types!

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+#![warn(clippy::pedantic)]
+#![allow(dead_code)]
+
+mod biguint;
+pub mod traits;
+
+pub use biguint::BigUInt;
+
+#[must_use]
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
